7545509fe07d946572964a599c664fe2c782c472
anosov34 <bookmake339@gmail.com>
2022-02-07 15:32:46 +0200

    first

---
+++ src/test/java/ru/hogvards/magicschool/MagicSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hogvards.magicschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
